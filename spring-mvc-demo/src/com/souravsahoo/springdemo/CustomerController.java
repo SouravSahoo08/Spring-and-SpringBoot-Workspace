ad69ad6bf8ccb8f1d50861c78c15dfc355422be4
Sourav Sahoo <jackysourav1994@gmail.com>
2022-10-20 19:24:04 +0530

    Initbinder preprocess added

--- spring-mvc-demo/src/com/souravsahoo/springdemo/CustomerController.java
+++ spring-mvc-demo/src/com/souravsahoo/springdemo/CustomerController.java
@@ -2,15 +2,29 @@ package com.souravsahoo.springdemo;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customerSection")
 public class CustomerController {
+
+	/**
+	 * initBinder preprocesses each web request to controller 
+	 * and trims leading and trailing whitespaces form the entry 
+	 * @param dataBinder
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 	
 	@RequestMapping("/showForm")
 	public String showform(Model model) {
